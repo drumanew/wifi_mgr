5aafab6508d6d78fafa6ff5a24d0ef67d607bcd0
Mikhail Khotko <mkhotko@plgn.ru>
2017-12-06 10:11:39 +0500

    Change WEB server port to 80; make it configurable

--- src/wifi_mgr_app.erl
+++ src/wifi_mgr_app.erl
@@ -5,11 +5,12 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
+  {ok, Port} = application:get_env(wifi_mgr, server_port),
   Dispatch =
     cowboy_router:compile([{'_', [{'_', wifi_mgr_handler, []}]}]),
   cowboy:start_http(wifi_mgr_http_listener,
                     100,
-                    [{port, 8080}],
+                    [{port, Port}],
                     [{env, [{dispatch, Dispatch}]}]),
   wifi_mgr_sup:start_link().
 
